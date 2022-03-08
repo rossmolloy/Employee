d81b8fc7665b018a9bf3b689cc2f8bdf2c8a35a5
ROSS MOLLOY - STUDENT <G00359442@gmit.ie>
2022-03-08 09:40:18 +0000

    Add title validation

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -1,9 +1,28 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class EmployeeTest {
 
     @Test
-    public void testEmployee() {
+    public void testValidEmployee() {
         Employee employee = new Employee("Mr", "John Doe", "1234567A", 123456789, "Full-time", 20);
+    }
+
+    @Test
+    public void testValidEmployeeWithTitleOfMrs() {
+        Employee employee = new Employee("Mrs", "Jane Doe", "1234567A", 123456789, "Full-time", 20);
+    }
+
+    @Test
+    public void testValidEmployeeWithTitleOfMs() {
+        Employee employee = new Employee("Ms", "Jane Doe", "1234567A", 123456789, "Full-time", 20);
+    }
+
+    @Test
+    public void testInvalidTitleThrowsException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> { new Employee("Invalid", "John Doe", "1234567A", 123456789, "Full-time", 20); });
+        assertEquals("Invalid title, must be 'Mr', 'Mrs', or 'Ms'", exception.getMessage());
     }
 }
