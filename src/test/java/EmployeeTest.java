ddf3c6c2f9bb9c7d5f7640d051da1ae4d6ef6f92
ROSS MOLLOY - STUDENT <G00359442@gmit.ie>
2022-03-08 09:55:33 +0000

    Add PPS ID validation

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -31,4 +31,10 @@ public class EmployeeTest {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> { new Employee("Mr", "Jo", "1234567A", 123456789, "Full-time", 20); });
         assertEquals("Name provided is not valid, must be between 3-25 characters in length", exception.getMessage());
     }
+
+    @Test
+    public void testEmployeeWithInvalidPpsIdThrowsException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> { new Employee("Mr", "John Doe", "123", 123456789, "Full-time", 20); });
+        assertEquals("Invalid PPS ID, must be 8 characters in length", exception.getMessage());
+    }
 }
