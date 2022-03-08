8640653ab5a50c1709c298368d3fa59a77645028
ROSS MOLLOY - STUDENT <G00359442@gmit.ie>
2022-03-08 09:47:22 +0000

    Add name validation

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -25,4 +25,16 @@ public class EmployeeTest {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> { new Employee("Invalid", "John Doe", "1234567A", 123456789, "Full-time", 20); });
         assertEquals("Invalid title, must be 'Mr', 'Mrs', or 'Ms'", exception.getMessage());
     }
+
+    @Test
+    public void testEmployeeWithShortNameThrowsException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> { new Employee("Mr", "Jo", "1234567A", 123456789, "Full-time", 20); });
+        assertEquals("Name provided is not valid, must be between 3-25 characters in length", exception.getMessage());
+    }
+
+    @Test
+    public void testEmployeeWithLongNameThrowsException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> { new Employee("Mr", "John Doooooooooooooooooooe", "1234567A", 123456789, "Full-time", 20); });
+        assertEquals("Name provided is not valid, must be between 3-25 characters in length", exception.getMessage());
+    }
 }
