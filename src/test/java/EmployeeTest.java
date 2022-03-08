ed958fcd11eaefee3f841e9b55400337725ae079
ROSS MOLLOY - STUDENT <G00359442@gmit.ie>
2022-03-08 09:22:46 +0000

    First commit with EmployeeTest

---
+++ src/test/java/EmployeeTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+
+    @Test
+    public void testEmployee() {
+        Employee employee = new Employee("Mr", "John Doe", "1234567A", 123456789, "Full-time", 20);
+    }
+}
