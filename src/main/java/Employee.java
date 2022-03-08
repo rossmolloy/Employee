c6ac95d6ca562076d14b324585673c9cb00d2f13
ROSS MOLLOY - STUDENT <G00359442@gmit.ie>
2022-03-08 09:29:17 +0000

    Add Employee class with constructor

---
+++ src/main/java/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    private String title;
+    private String name;
+    private String ppsId;
+    private Integer phone;
+    private String employmentType;
+    private Integer age;
+
+    public Employee(String title, String name, String ppsId, Integer phone, String employmentType, Integer age) {
+        this.title = title;
+        this.name = name;
+        this.ppsId = ppsId;
+        this.phone = phone;
+        this.employmentType = employmentType;
+        this.age = age;
+    }
+}
